dc5187e0131aeae8a0690d619be0e508479b7806
Gianmarco965 <gbendezu@iapica.edu.pe>
2017-02-26 00:27:37 -0500

    update26

--- js/modalmin.js
+++ js/modalmin.js
@@ -1,24 +1,25 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 
+// Get the button that opens the modal
+
+var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+function open__modal() {
+    modal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
-function span__close() {
+span.onclick = function() {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-    }     
-
+    }
 }
-
-
- function modal__open(){
-      modal.style.display = "block";     
-    }  
-          
